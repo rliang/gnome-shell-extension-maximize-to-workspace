0c558dacbf9e92e18e8b8e6d1abe1e9a07548e3b
LFOM <4441874-lfomartins@users.noreply.gitlab.com>
2020-09-08 01:54:48 -0300

    Added preferences and fixes.

--- extension.js
+++ extension.js
@@ -1,5 +1,25 @@
 'use strict';
 const Meta = imports.gi.Meta;
+const GLib = imports.gi.GLib;
+const ExtensionUtils = imports.misc.extensionUtils;
+
+const Me = ExtensionUtils.getCurrentExtension();
+const Prefs = Me.imports.prefs;
+
+const _handles = [];
+const _history = {};
+
+let _checkWorkspace;
+let _expand;
+let _expandWorkspace;
+let _isStarting;
+let _lastExpand;
+let _lastHistory;
+let _lastRestore;
+let _settings;
+let _restore;
+let _restoreHistory;
+let _singleMonitor;
 
 /* This has been tested with both static and dynamic workspaces.  It
  * works better with dynamic workspaces, but can work well if you have
@@ -11,23 +31,49 @@ const Meta = imports.gi.Meta;
  *  target a specific desktop when none are empty
  *  (don't) skip first desktop
  */
-function check(act) {
+function checkMaximize(act) {
   const metaWindow = act.meta_window;
-  const workspaceManager = metaWindow.get_display().get_workspace_manager();
-  // log('maximize-to-workspace check | window: ' + metaWindow.title);
+  log('maximize-to-workspace checkMaximize | start: ' + _isStarting);
 
   // abort if it is not a normal window nor maximized
   if (
+    !metaWindow ||
+    metaWindow.on_all_workspaces ||
     metaWindow.window_type !== Meta.WindowType.NORMAL ||
     metaWindow.get_maximized() !== Meta.MaximizeFlags.BOTH
     ) {
       return;
-    }
+  }
+  log('maximize-to-workspace checkMaximize | window: ' + metaWindow.title);
+
+  if (
+    _singleMonitor &&
+    !metaWindow.is_on_primary_monitor()
+    ) {
+      return;
+  }
+
+  const workspaceManager = metaWindow.get_display().get_workspace_manager();
+  if (!workspaceManager) {
+    return;
+  }
 
   let windowList = metaWindow.get_workspace()
     .list_windows()
-    .filter(w => w !== metaWindow && !w.is_on_all_workspaces());
+    .filter(w =>
+      w !== metaWindow &&
+      !w.is_on_all_workspaces() &&
+      w.get_monitor() == metaWindow.get_monitor()
+    );
   
+  
+  if (_restore && _restoreHistory) {
+    _history[metaWindow.get_id()] = metaWindow.get_workspace().index();
+    log('maximize-to-workspace checkMaximize | windowId: ' + metaWindow.get_id());
+    log('maximize-to-workspace checkMaximize | _history.length: ' +
+      Object.keys(_history).length);
+  }
+
   // continue only if exists any other window in the current workspace
   if (windowList && windowList.length > 0) {
     // put on last workspace if all else fails (OO)
@@ -35,7 +81,7 @@ function check(act) {
     if (lastWorkspace < 1) {
       lastWorkspace = 1;
     }
-      
+
     // always start with the second workspace (OO)
     let emptyWorkspace = 1;
 
@@ -44,54 +90,204 @@ function check(act) {
       windowList = workspaceManager.get_workspace_by_index(emptyWorkspace)
         .list_windows()
         .filter(w => !w.is_on_all_workspaces());
+      log('maximize-to-workspace checkMaximize | w.length: ' + windowList.length);
       if (windowList.length < 1) {
         break;
         }
       emptyWorkspace++;
     }
+
+    log('maximize-to-workspace checkMaximize | emptyWorkspace: ' + emptyWorkspace);
+
     // don't try to move it if we're already here (break recursion)
     if (emptyWorkspace == metaWindow.get_workspace().index()) {
       return;
     }
 
-    // move windows and activate workspace
-    metaWindow.change_workspace_by_index(emptyWorkspace, false);
-    workspaceManager.get_workspace_by_index(emptyWorkspace)
-      .activate(global.get_current_time());
+    if (_expand) {
+      if (_expandWorkspace == null || _isStarting) {
+        _expandWorkspace = emptyWorkspace;
+      }
+      emptyWorkspace = _expandWorkspace;
+    }
+
+    // move window and activate workspace
+    moveWindow(metaWindow, emptyWorkspace, workspaceManager);
+
+    log('maximize-to-workspace checkMaximize | end window: ' + metaWindow.title);
   }
 }
 
-const _handles = [];
+function moveWindow(metaWindow, workspace, workspaceManager) {
+  log('maximize-to-workspace moveWindow start | window: ' + metaWindow.title);
+  metaWindow.change_workspace_by_index(workspace, true);
+  if (_isStarting) {
+    workspaceManager.get_workspace_by_index(workspace)
+      .activate(global.get_current_time());
+    log('maximize-to-workspace moveWindow end | window: ' + metaWindow.title);
+    return;
+  }
+  GLib.timeout_add(
+    GLib.PRIORITY_LOW,
+    300,
+    () => {
+      workspaceManager.get_workspace_by_index(workspace)
+        .activate_with_focus(metaWindow, global.get_current_time());
+      log('maximize-to-workspace moveWindow end | window: ' + metaWindow.title);
+      return GLib.SOURCE_REMOVE;
+  });
+}
+
+function checkRestore(act) {
+  const metaWindow = act.meta_window;
+  log('maximize-to-workspace checkRestore | window: ' + metaWindow.title);
+
+  if (
+    !metaWindow ||
+    metaWindow.on_all_workspaces ||
+    !_restore ||
+    metaWindow.window_type !== Meta.WindowType.NORMAL
+    ) {
+    return;
+  }
+
+  if (
+    _singleMonitor &&
+    !metaWindow.is_on_primary_monitor()
+    ) {
+      checkHistory(metaWindow);
+      return;
+  }
+
+  const workspaceManager = metaWindow.get_display().get_workspace_manager();
+  let restoreWorkspace = null;
+
+  if (_restoreHistory) {
+    restoreWorkspace = checkHistory(metaWindow);
+  } else {
+    restoreWorkspace = 0;
+  }
+  log('maximize-to-workspace checkRestore | restoreWorkspace.1: ' + restoreWorkspace);
+
+  if (
+    restoreWorkspace != null &&
+    restoreWorkspace < workspaceManager.get_n_workspaces() &&
+    restoreWorkspace != metaWindow.get_workspace().index()
+    ) {
+    log('maximize-to-workspace checkRestore | restoreWorkspace.2: ' + restoreWorkspace);
+    moveWindow(metaWindow, restoreWorkspace, workspaceManager);
+  }
+}
+
+function runDelayed(act, type) {
+  GLib.timeout_add(
+    GLib.PRIORITY_LOW,
+    500,
+    () => {
+      type ? checkMaximize(act) : checkRestore(act);
+      return GLib.SOURCE_REMOVE;
+  });
+}
+
+function checkHistory(metaWindow) {
+  if (!_history) {
+    return null;
+  }
+  let index = metaWindow.get_id();
+  index = _history[index];
+  log('maximize-to-workspace checkHistory | index: ' + index);
+  if (index != undefined) {
+    delete _history[index];
+    return index;
+  }
+  return null;
+}
+
+function clearHistory() {
+  log('maximize-to-workspace clearHistory | history: ' + JSON.stringify(_history));
+  if (Object.keys(_history).length) {
+    Object.keys(_history).forEach(key => delete _history[key]);
+  }
+}
+// update variables from settings
+function updateSettings() {
+  log('maximize-to-workspace updateSettings | lastRestore: ' + _lastRestore +
+    '  lastHistory: ' + _lastHistory +
+    '  lastExpand: ' + _lastExpand);
+  _singleMonitor = _settings.get_boolean(Prefs.SETTINGS_SINGLE_MONITOR);
+  _restore = _settings.get_boolean(Prefs.SETTINGS_RESTORE);
+  _expand = _settings.get_boolean(Prefs.SETTINGS_EXPAND);
+  _restoreHistory = _settings.get_enum(Prefs.SETTINGS_RESTORE_MODE);
+
+  if (_expand != _lastExpand) {
+    _expandWorkspace = null;
+    _lastExpand = _expand;
+  }
+
+  if ((_restoreHistory != _lastHistory) || (_restore != _lastRestore)) {
+    clearHistory();
+    _lastRestore = _restore;
+    _lastHistory = _restoreHistory;
+  }
+
+  log('maximize-to-workspace updateSettings | restore: ' + _restore +
+    '  singleMonitor: ' + _singleMonitor +
+    '  restoreHist: ' + _restoreHistory +
+    '  expand: ' + _expand +
+    ' _history: ' + Object.keys(_history).length);
+}
 
 function enable() {
-  //log('maximize-to-workspace enable');
+  _isStarting = true;
+  // log('maximize-to-workspace enable');
+
+  // load settings and connect signal
+  _settings = ExtensionUtils.getSettings();
+  _handles.push(_settings.connect('changed', updateSettings));
+  updateSettings();
+  log('maximize-to-workspace enable | _handles: ' + _handles.length +
+    ' _history: ' + Object.keys(_history).length);
 
   // when started, check all current windows
-  global.get_window_actors().forEach(check);
+  global.get_window_actors().forEach(checkMaximize);
 
-  // handle 'map' (new windows)
-  _handles.push(global.window_manager.connect('map', (_, act) => check(act)));
+  // handle signal 'map' (new windows)
+  _handles.push(global.window_manager.connect('map', (_, act) => runDelayed(act, 1)));
 
   // handle signal 'size-change'
   _handles.push(global.window_manager.connect('size-change', (_, act, change) => {
     if (change === Meta.SizeChange.MAXIMIZE) {
-      check(act);
+      runDelayed(act, 1);
+    } else if (change === Meta.SizeChange.UNMAXIMIZE) {
+      runDelayed(act, 0);
     }
   }));
 
   // handle signal 'switch-workspace'
-  _handles.push(global.window_manager.connect('switch-workspace', () => {
-    const acts = global.get_window_actors()
-      .filter(a => a.meta_window.has_focus());
-    if (acts.length) {
-      check(acts[0]);
-    }
-  }));
+  if (_checkWorkspace) {
+    _handles.push(global.window_manager.connect('switch-workspace', () => {
+      const acts = global.get_window_actors()
+        .filter(a => a.meta_window.has_focus());
+      if (acts.length) {
+        checkMaximize(acts[0]);
+      }
+    }));
+  }
+
+  _isStarting = false;
 }
 
 function disable() {
   // log('maximize-to-workspace disable');
 
   // disconnect from all signals
-  _handles.splice(0).forEach(h => global.window_manager.disconnect(h));
+  _handles.splice(0).forEach((key, i) => {
+    if (i) {
+      global.window_manager.disconnect(key);
+    } else {
+      _settings.disconnect(key);
+    }
+  });
+
+  clearHistory();
 }
---
+++ prefs.js
@@ -0,0 +1,85 @@
+'use strict';
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+var SETTINGS_SINGLE_MONITOR = 'single-monitor';
+var SETTINGS_RESTORE = 'restore';
+var SETTINGS_RESTORE_MODE = 'restore-mode';
+var SETTINGS_EXPAND = 'expand';
+var SETTINGS_RESTORE_TO_FIRST = 'restore-to-first';
+var SETTINGS_RESTORE_HISTORY = 'restore-history';
+
+var Settings = class DashToDock_Settings {
+
+  constructor() {
+    this._settings = ExtensionUtils.getSettings();
+
+    this._builder = new Gtk.Builder();
+    this._builder.add_from_file(Me.path + '/Settings.ui');
+
+    this.widget = new Gtk.ScrolledWindow({ hscrollbar_policy: Gtk.PolicyType.NEVER });
+    this._notebook = this._builder.get_object('settings_notebook');
+    this.widget.add(this._notebook);
+
+    this._bindSettings();
+
+    this._builder.connect_signals_full(this._connector.bind(this));
+  }
+
+  _connector(builder, object, signal, handler) {
+     
+    const SignalHandler = {
+      restore_to_first_radio_button_toggled_cb(button) {
+        if (button.get_active())
+          this._settings.set_enum(SETTINGS_RESTORE_MODE, 0);
+      },
+
+      restore_history_radio_button_toggled_cb(button) {
+        if (button.get_active())
+          this._settings.set_enum(SETTINGS_RESTORE_MODE, 1);
+      }
+    }
+
+    object.connect(signal, SignalHandler[handler].bind(this));
+  }
+
+  _bindSettings() {
+    
+    this._settings.bind(SETTINGS_SINGLE_MONITOR,
+      this._builder.get_object('single_monitor_switch'),
+      'active',
+      Gio.SettingsBindFlags.DEFAULT);
+    this._settings.bind(SETTINGS_EXPAND,
+      this._builder.get_object('expand_switch'),
+      'active',
+      Gio.SettingsBindFlags.DEFAULT);
+    this._settings.bind(SETTINGS_RESTORE,
+      this._builder.get_object('restore_switch'),
+      'active',
+      Gio.SettingsBindFlags.DEFAULT);
+              
+    this._settings.bind(SETTINGS_RESTORE,
+      this._builder.get_object('restore_mode_box'),
+      'sensitive',
+      Gio.SettingsBindFlags.GET);
+    let expandModeRadioButtons = [
+      this._builder.get_object('restore_to_first_radio_button'),
+      this._builder.get_object('restore_history_radio_button'),
+    ];
+    expandModeRadioButtons[
+      this._settings.get_enum(SETTINGS_RESTORE_MODE)
+    ].set_active(true);
+  }
+};
+
+function init() {
+}
+
+function buildPrefsWidget() {
+  let settings = new Settings();
+  let widget = settings.widget;
+  widget.show_all();
+  return widget;
+}
